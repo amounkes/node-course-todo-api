0bc12b2eb358128a3b8543063146f4dc9048238a
Amy <amounkes@redventures.com>
2018-09-25 19:11:59 -0400

    Add queries challenge

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,52 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5ba8152479aac1c2ad32e548';
+
+// if (ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log("Todos", todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log("Todo", todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('ID not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+// User.findById(id).then((user) => {
+//     if (!user) {
+//         return console.log('User not found');
+//     }
+//     console.log(JSON.stringify(user, undefined, 2));
+// }, (e) => {
+//     console.log(e);
+// });
+
+User.findById('5ba8152479aac1c2ad32e548').then((user) => {
+    if (!user) {
+      return console.log('Unable to find user');
+    }
+  
+    console.log(JSON.stringify(user, undefined, 2));
+  }, (e) => {
+    console.log(e);
+  });
+
+
+
