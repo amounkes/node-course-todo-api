a8fa4473a090cfd1cc328f2b59d0352415dd1ffe
Amy <amounkes@redventures.com>
2018-09-26 21:02:58 -0400

    Set up seperate test and dev dbs

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -5,9 +5,16 @@ const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
 // Todo.remove({}).then((result) => {
-//     console.log(result);
+//   console.log(result);
 // });
 
-Todo.findByIdAndRemove('5bac039449543b5a46ecc034').then((todo) => {
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+// Todo.findOneAndRemove({_id: '57c4610dbb35fcbf6fda1154'}).then((todo) => {
+//
+// });
+
+Todo.findByIdAndRemove('57c4610dbb35fcbf6fda1154').then((todo) => {
     console.log(todo);
 });
